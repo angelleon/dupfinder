1651a7dadcc9685dd09d11c989158bdde230542a
Angel Leon <luianglenlop@gmail.com>
2022-08-02 01:44:41 -0500

    adding cli args parser

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,24 @@
-use std::env;
+//use std::env;
 pub mod dupfinder;
 
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct CliOptions {
+    #[clap(value_parser)]
+    path: Vec<std::path::PathBuf>,
+}
+
 fn main() {
-    let mut args: Vec<String> = env::args().collect();
-    args.remove(0);
-    let mut finder = dupfinder::DupFinder::new(args);
+    let cli_args = CliOptions::parse();
+    //for _ in 0..cli_args.count {
+    println!("{:?}", cli_args.path);
+    println!("{:?}", cli_args.path.len());
+    //}
+    //panic!();
+    //let mut args: Vec<String> = env::args().collect();
+    //args.remove(0);
+    let mut finder = dupfinder::DupFinder::new(cli_args.path);
     finder.run();
 }
