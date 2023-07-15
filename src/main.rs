1bc0cbc46b19a2c7873b5bdf25a393d739188d3e
Angel Leon <luianglenlop@gmail.com>
2023-07-15 16:04:19 -0600

    added verbose flag

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,16 @@ use clap::Parser;
 struct CliOptions {
     #[clap(value_parser)]
     path: Vec<std::path::PathBuf>,
+    #[arg(short = 's', long, default_value_t = 0)]
+    min_size: u64,
+    #[arg(short = 'S', long, default_value_t = std::u64::MAX)]
+    max_size: u64,
+    #[arg(short = 'v', long, default_value_t = false)]
+    verbose: bool
 }
 
 fn main() {
     let cli_args = CliOptions::parse();
-    let mut finder = dupfinder::DupFinder::new(cli_args.path);
+    let mut finder = dupfinder::DupFinder::new(cli_args.path, cli_args.min_size, cli_args.max_size, cli_args.verbose);
     finder.run();
 }
